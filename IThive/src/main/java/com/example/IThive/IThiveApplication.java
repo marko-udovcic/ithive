ae90c947ac3c0ae79ea2c49679c83d212411ed0a
marko udovcic <markoudovcic53@gmail.com>
2024-11-11 11:13:29 +0100

    edit IThiveApp.java

--- IThive/src/main/java/com/example/IThive/IThiveApplication.java
+++ IThive/src/main/java/com/example/IThive/IThiveApplication.java
@@ -8,6 +8,6 @@ public class IThiveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IThiveApplication.class, args);
-		System.out.println("hello development");
+		System.out.println("hello developme");
 	}
 }
