d4ab590bb09f0b7e9c53308c9d98c7cd3c452769
marko udovcic <markoudovcic53@gmail.com>
2024-11-30 13:25:57 +0100

    chore: Inital setup for dev branch

--- IThive/src/main/java/com/example/IThive/IThiveApplication.java
+++ IThive/src/main/java/com/example/IThive/IThiveApplication.java
@@ -9,6 +9,6 @@ public class IThiveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IThiveApplication.class, args);
-		System.out.println("hello main");
+		System.out.println("hello dev");
 	}
 }
