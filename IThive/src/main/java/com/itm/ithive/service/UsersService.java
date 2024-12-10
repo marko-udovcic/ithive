501458d9074da205234be78a62ee4e30c668fefd
unknown <aperan09@gmail.com>
2024-12-10 15:40:20 +0100

    delete: remove authenticateUser in UserService

--- IThive/src/main/java/com/itm/ithive/service/UsersService.java
+++ IThive/src/main/java/com/itm/ithive/service/UsersService.java
@@ -1,6 +1,7 @@
 package com.itm.ithive.service;
 
 
+import com.itm.ithive.exceptions.UserAlreadyExisting;
 import com.itm.ithive.model.Users;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -8,10 +9,7 @@ import java.util.List;
 
 public interface UsersService {
     public Users createUsers(Users user);
-
-
-    public Users registerUser(Users user);
-    public boolean authenticateUser (String username, String password);
+    public Users registerUser(Users user) throws UserAlreadyExisting;
     public List<Users> listAll();
 
 }
