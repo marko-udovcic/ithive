2325c3fbfa5d1314136db111f37af494ab58749d
marko udovcic <markoudovcic53@gmail.com>
2024-12-13 11:15:53 +0100

    refactor: update CustomSuccessHandler for redirecting users

--- IThive/src/main/java/com/itm/ithive/security/CustomSuccessHandler.java
+++ IThive/src/main/java/com/itm/ithive/security/CustomSuccessHandler.java
@@ -24,7 +24,7 @@ public class CustomSuccessHandler implements AuthenticationSuccessHandler {
                 .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(Role.User.name()));
 
 
-        response.sendRedirect("/home");
+        response.sendRedirect("/profile");
 
 
     }
