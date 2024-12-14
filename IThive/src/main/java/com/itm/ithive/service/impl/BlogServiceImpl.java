0a63114384399a1a48d9971d35370a3cac6469ec
unknown <aperan09@gmail.com>
2024-12-14 12:54:28 +0100

    feat: add listing all Blogs and searching Blogs by User

--- IThive/src/main/java/com/itm/ithive/service/impl/BlogServiceImpl.java
+++ IThive/src/main/java/com/itm/ithive/service/impl/BlogServiceImpl.java
@@ -1,10 +1,13 @@
 package com.itm.ithive.service.impl;
 
 import com.itm.ithive.model.Blog;
+import com.itm.ithive.model.Users;
 import com.itm.ithive.repository.BlogRepository;
 import com.itm.ithive.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +17,15 @@ public class BlogServiceImpl implements BlogService {
     @Override
     public Blog createBlog(Blog blog) {
         return blogRepository.save(blog);
+    }
+
+    @Override
+    public List<Blog> listAll(){
+        return blogRepository.findAll();
+    }
+
+    @Override
+    public List<Blog> findBlogByUser(Users user) {
+        return blogRepository.findByUser(user);
     }
 }
