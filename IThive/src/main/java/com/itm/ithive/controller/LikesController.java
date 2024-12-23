367262a6b68f76b5688e550621217c63aa14d3cc
unknown <aperan09@gmail.com>
2024-12-23 23:05:11 +0100

    feat: add post mapp handler that saves likes to a blog defined by its id, also has implemented toggle logic for likes and dislikes

--- IThive/src/main/java/com/itm/ithive/controller/LikesController.java
+++ IThive/src/main/java/com/itm/ithive/controller/LikesController.java
@@ -1,22 +1,28 @@
 package com.itm.ithive.controller;
 
 
+import com.itm.ithive.model.Blog;
 import com.itm.ithive.model.Likes;
+import com.itm.ithive.service.BlogService;
 import com.itm.ithive.service.LikesService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@AllArgsConstructor
+@Controller
 @RequestMapping("/likes")
 public class LikesController {
 
-    private final LikesService likesService;
+    @Autowired
+    private LikesService likesService;
 
-    public LikesController(LikesService likesService) {
-        this.likesService = likesService;
-    }
+    @Autowired
+    private BlogService blogService;
 
     @GetMapping
     public List<Likes> findAllLikes() {
@@ -36,5 +42,23 @@ public class LikesController {
     @DeleteMapping("/{id}")
     public void deleteLike(@PathVariable long id) {
         likesService.deleteLike(id);
+    }
+
+    @PostMapping("/addLikes")
+    public String addLikes(@RequestParam String username,
+                           @RequestParam Long blogId,
+                           @RequestParam boolean doILike,
+                           HttpServletRequest request){
+
+
+        Blog blog = blogService.findBlogById(blogId);
+        if (doILike){
+            likesService.deleteLikeByBlog(blog);
+        }
+        else{
+            likesService.addLikeByBlog(blog);
+        }
+
+        return "redirect:/blog/blogView/" + blogId;
     }
 }
