53071f86cd536d6fc89132481daf6945cf93b3af
marko udovcic <markoudovcic53@gmail.com>
2024-12-27 22:49:17 +0100

    chore: add jpa spec

--- IThive/src/main/java/com/itm/ithive/repository/BlogRepository.java
+++ IThive/src/main/java/com/itm/ithive/repository/BlogRepository.java
@@ -3,12 +3,14 @@ package com.itm.ithive.repository;
 import com.itm.ithive.model.Blog;
 import com.itm.ithive.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface BlogRepository extends JpaRepository<Blog, Long> {
+public interface BlogRepository extends JpaRepository<Blog, Long>, JpaSpecificationExecutor<Blog> {
     List<Blog> findByUser(Users user);
+
     Blog findBlogById(Long id);
 }
